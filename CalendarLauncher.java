093e32c379e6c769d66c449638b04ad0634094d0
dekuRockShooter <dekuRockShooter@gmail.com>
2017-01-15 03:06:30 -0800

    Start project

---
+++ CalendarLauncher.java
@@ -0,0 +1,64 @@
+import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventType;
+import javafx.event.EventHandler.*;
+import javafx.event.EventHandler;
+import javafx.event.*;
+import javafx.event.Event.*;
+import javafx.geometry.Insets;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn.CellEditEvent;
+import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ButtonType;
+import javafx.util.Callback;
+
+import java.util.Optional;
+import java.util.Calendar;
+
+
+public class CalendarLauncher extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        final Label label = new Label("Address Book");
+        label.setFont(new Font("Arial", 20));
+        final VBox vbox = new VBox();
+        vbox.setSpacing(5);
+        vbox.setPadding(new Insets(10, 0, 0, 10));
+        vbox.getChildren().addAll(label);
+        Scene scene = new Scene(vbox);
+        stage.setTitle("Calendar");
+        stage.setWidth(300);
+        stage.setHeight(500);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+}
